04d4b564c2c29f1cf4594731a80acc4273b0d87c
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-01-18 21:36:51 -0600

    added seeds file. perfected the schema file to make it successful in mysql. added roles function, and individual roles.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var util = require("util");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,15 +11,20 @@ var connection = mysql.createConnection({
     user: "root",
 
     password: "B055p0w3r!",
-    database: "schema_db"
+    database: "employees_db"
 });
+
+connection.query = util.promisify(connection.query);
 
 connection.connect(function (err) {
     if (err) throw err;
+    
     start();
 });
 
-// function which prompts the user for what action they should take
+
+
+// function which asks the user for what they should take
 async function start() {
     var answer = await inquirer.prompt
         (
@@ -30,12 +36,125 @@ async function start() {
             }
         )
 
-    // based on their answer, either call the bid or the post functions
     if (answer.mainMenu === "Departments") {
-        // add functions for department
+        departments();
+
     } else if (answer.mainMenu === "Roles") {
-        // add functions for roles
+        roles();
+
     } else if (answer.mainMenu === "Employees"){
+        employees();
+
+    } else {
+        connection.end();
+
+    }
+}
+
+// function for departments and
+async function departments() {
+    // var departmentResults = await connection.query("SELECT * FROM schema_db.department");
+    // console.table(departmentResults);
+    var answer = await inquirer.prompt
+        (
+            {
+                name: "department",
+                type: "list",
+                message: "choose the department you want to look at.",
+                choices: ["Sales", "Service", "Exit"]
+            }
+        )
+
+    // 
+    if (answer.department === "Sales") {
+        // add functions for Sales
+        
+    } else if (answer.department === "Service") {
+        // add functions for Service
+        
+    } else {
+        connection.end();
+    }
+}
+// add function for sales employees. will have subcategory of sales director,then sales managers, then sales employees and
+
+
+
+// add function for sales
+
+
+
+
+
+
+
+// add function for service
+
+
+
+
+
+
+
+
+
+// function for roles
+async function roles() {
+    var answer = await inquirer.prompt
+        (
+            {
+                name: "roles",
+                type: "list",
+                message: "What roles do you want to look at?",
+                choices: ["Technicians", "Service Advisors", "Salesmen", "Managers", "Show All Employees", "Exit"]
+            }
+        )
+
+    // 
+    if (answer.mainMenu === "Technicians") {
+        // add functions for 
+    } else if (answer.mainMenu === "Service Advisors") {
+        // add functions for 
+    } else if (answer.mainMenu === "Salesmen"){
+        // add function for 
+    } else if (answer.mainMenu === "Managers"){
+        // add function for 
+    }else if (answer.mainMenu === "Show All Employees"){
+        // add function for 
+    } else {
+            connection.end();
+        }
+    }
+
+
+// add functions for every role.
+
+
+
+
+
+
+
+
+// function for employees
+
+async function employees() {
+    var answer = await inquirer.prompt
+        (
+            {
+                name: "mainMenu",
+                type: "list",
+                message: "Main Menu",
+                choices: ["", "", "", "Exit"]
+            }
+        )
+
+    // based on their answer, either call the bid or the post functions
+    if (answer.mainMenu === "") {
+        // add functions for department
+    } else if (answer.mainMenu === "") {
+        // add functions for roles
+    } else if (answer.mainMenu === ""){
         // add function for employees.
     } else {
         connection.end();
